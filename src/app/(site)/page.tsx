d6f1004b2a3696a426faa3b016dc2f472a6858e4
Mubashar217 <muhammadmubasharoffical@gmail.com>
2025-06-02 12:39:01 +0500

    add

---
+++ src/app/(site)/page.tsx
@@ -0,0 +1,16 @@
+import Home from "@/components/Home";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "NextCommerce | Nextjs E-commerce template",
+  description: "This is Home for NextCommerce Template",
+
+};
+
+export default function HomePage() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
